c1ff6635fcd3424f9a01ab28093dd883e7ab12f2
Itai Spigelman <itai10020@gmail.com>
2021-04-09 11:31:40 +0300

    ex2

--- ex2.asm
+++ ex2.asm
@@ -3,3 +3,13 @@
 .section .text
 _start:
 #your code here
+    movq $source, %rsi
+    movq $destination, %rdi
+    
+    movl (num), %ecx
+    test %ecx, %ecx
+    jl end
+    
+    rep movsb
+    
+end:
--- playground2.asm
+++ playground2.asm
@@ -3,11 +3,22 @@
 .section .data
 source: .int 6,12,9,8
 destination: .int 1,2,3,4 # expected: .int 6,12,3,4
+num: .int 8
 
 .section .text    
-ex3:
+ex2:
+    movq $source, %rsi
+    movq $destination, %rdi
     
-
+    movl (num), %ecx
+    test %ecx, %ecx
+    jl end
+    
+    rep movsb
+    
+end:
+    ret
+      
 main:
     movq %rsp, %rbp #for correct debugging
     call ex2
