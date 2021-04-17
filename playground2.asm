29a7b895ea67b3ebd387694813188c53ebd701b9
DorShacham <63207955+DorShacham@users.noreply.github.com>
2021-04-17 18:32:13 +0300

    Update playground2.asm

--- playground2.asm
+++ playground2.asm
@@ -1,25 +1,34 @@
-.global main
+#.global _start
+.global main #need to delete
 
-.section .data
-source: .int 6,12,9,8
-destination: .int 1,2,3,4 # expected: .int 6,12,3,4
+.section .data #have to delete this part
 num: .int 8
+source: .int 6,12,9,8
+destination: .int 1,2,3,4
 
-.section .text    
-ex2:
-    movq $source, %rsi
-    movq $destination, %rdi
-    
-    movl (num), %ecx
-    test %ecx, %ecx
-    jl end
-    
-    rep movsb
-    
-end:
-    ret
-      
+
+.section .text
+#_start:
 main:
-    movq %rsp, %rbp #for correct debugging
-    call ex2
-    ret
+    movq %rsp, %rbp #for correct debugging #need to deleate
+#your code here
+    xorq %rax,%rax
+    cmpl $0, num #if num<=0 do nothing
+    jle end
+    movq $source, %rbx 
+    movq $destination, %rdx
+  loop:
+       movb (%rbx), %al 
+       movb %al, (%rdx) #moves 1 byte from source to dest
+       
+       incq %rbx #updates the values
+       incq %rdx
+       decl num
+       je end #if num=0 we finished
+       jmp loop #else continue
+       
+    
+    
+  end:
+    ret #need to delete
+    
